38b8ae9d7033677f49efc2123e8f8ea9525070a9
EugeneyCh <eugeney@ua.fm>
2025-03-02 14:57:09 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,3 @@
-// import SearchBox from "./components/SearchBox/SearchBox";
-// import ContactList from "./components/ContactList/ContactList";
-// import ContactForm from "./components/ContactForm/ContactForm";
-
-// import s from "./App.module.css";
-// import { useEffect } from "react";
-// import { fetchContacts } from "./redux/contacts/operations";
-// import { useDispatch } from "react-redux";
-
-// function App() {
-//   const dispatch = useDispatch();
-
-//   useEffect(() => {
-//     const abortController = new AbortController();
-//     dispatch(fetchContacts({ signal: abortController.signal }));
-//     return () => {
-//       abortController.abort();
-//     };
-//   }, [dispatch]);
-
-//   return (
-//     <div className={s.flexContainer}>
-//       <h1>Phonebook</h1>
-//       <ContactForm />
-//       <SearchBox />
-//       <ContactList />
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/HomePage/Home";
 import Contacts from "./pages/ContactsPage/ContactsPage";
