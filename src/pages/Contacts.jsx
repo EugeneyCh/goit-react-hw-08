73c46b82ba625dfa7a6ee62202395506d2143e76
EugeneyCh <eugeney@ua.fm>
2025-05-18 19:51:08 +0300

    Update Contacts.jsx

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,11 +1,9 @@
-import { useSelector } from "react-redux";
-import ContactList from "../components/ContactList/ContactList";
-import { selectLoading } from "../redux/contacts/selectors";
-// import { useEffect } from "react";
-import { Helmet } from "react-helmet";
+import { useSelector } from 'react-redux';
+import ContactList from '../components/ContactList/ContactList';
+import { selectLoading } from '../redux/contacts/selectors';
+import { Helmet } from 'react-helmet';
 
 const Contacts = () => {
-  // const dispatch = useDispatch();
   const isLoading = useSelector(selectLoading);
 
   // useEffect(() => {
@@ -17,7 +15,7 @@ const Contacts = () => {
       <Helmet>
         <title>Your contacts</title>
       </Helmet>
-      <div>{isLoading && "Request in progress..."}</div>
+      <div>{isLoading && 'Request in progress...'}</div>
       <ContactList />
     </>
   );
